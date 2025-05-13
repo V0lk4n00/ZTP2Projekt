04ba692904368a99b77b5596f59caa478740849d
Your Name <you@example.com>
2025-05-13 14:38:17 +0200

    First commit. Controller and static record repository

---
+++ app/src/Controller/HelloController.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Hello controller.
+ */
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+/**
+ * Class HelloController.
+ */
+#[Route('/hello')]
+class HelloController extends AbstractController
+{
+    /**
+     * Index action.
+     *
+     * @param string $user_name User input
+     *
+     * @return Response HTTP Response
+     */
+    #[Route(
+        '/{user_name}',
+        name: 'hello_index',
+        requirements: ['user_name' => '[a-zA-Z]+'],
+        defaults: ['user_name' => 'world'],
+        methods: ['GET']
+    )]
+    public function index(string $user_name): Response
+    {
+        return $this->render(
+            'hello/index.html.twig',
+            ['user_name' => $user_name]
+        );
+    }
+}
